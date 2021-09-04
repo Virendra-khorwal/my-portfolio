a1cb56f1b6a253e69131fcfb6ff1c585ece74c8a
Virendra-Khorwal <virendrakhorwalvk@gmail.com>
2021-09-05 00:00:45 +0530

    React

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Calculator App with Javascript",
+    subtitle: "Javascript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./Calculator.jpg",
+    link: "https://github.com/Virendra-khorwal/Online-Calculator-with-Javascript",
+  },
+  {
+    title: "Calculator with Kotlin",
+    subtitle: "Kotlin and Android Studio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./Basic Calculator App.jpeg",
+    link: "https://github.com/Virendra-khorwal/Basice-Calculator-app",
+  },
+  
+];
+
